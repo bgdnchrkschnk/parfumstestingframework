98459ffe49a5619dc4966323f3320c9d0d38dff9
bgdnchrkschnk <bgdnchrkschnk@gmail.com>
2023-08-28 02:28:07 +0300

    first commit!

--- t.py
+++ t.py
@@ -1,7 +1,17 @@
-from io import StringIO
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from locators.catalog_item import CatalogItem
 
-s = StringIO()
-s.write("scabhbjhbcs")
-s.write("657652367766732")
-a = s.getvalue()
-print(a, type(a))
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 20)
+driver.get(url="https://parfums.ua/ua/category/lady")
+
+
+first_element = wait.until(EC.element_to_be_clickable((By.XPATH, str(CatalogItem.get_element_by_title_xpath("Versace Bright Crystal")))))
+second_element = first_element.find_element(By.XPATH, ".//ancestor::div[@class='info-product-wrapper']//span[text() and @class='price_item']")
+print(type(first_element), type(second_element))
+
+
+# другий теж буде обʼєктом webdriverwait? тобто його можна юзати далі в коді, і на нього теж буде працювати очікування 20 сек?
--- web_elements/catalog_item.py
+++ web_elements/catalog_item.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.by import By
 from locators.catalog_item import CatalogItem
 
-
 class CatalogItemElement:
     def __init__(self, web_element):
         self.__web_element = web_element
@@ -10,4 +9,3 @@ class CatalogItemElement:
     def price(self):
         element = self.__web_element.find_element(By.XPATH, str(CatalogItem.get_element_price_by_title_xpath()))
         return element.text
-
